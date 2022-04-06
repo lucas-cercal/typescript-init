a80ac447e8e9736fcb420d14e15e74dcbcf6784c
Lucas Cercal <lucascercal.fontes@gmail.com>
2022-04-06 14:04:47 -0300

    Interfaces e Métodos

--- interfaces/interfaces.js
+++ interfaces/interfaces.js
@@ -7,9 +7,13 @@ function mudarNome(pessoa) {
 }
 const pessoa = {
     nome: "Lucas",
-    idade: 22
+    idade: 22,
+    saudar(sobrenome) {
+        console.log("Olá, meu nome é " + this.nome + " " + sobrenome);
+    }
 };
 saudarComOla(pessoa);
 mudarNome(pessoa);
 saudarComOla(pessoa);
-saudarComOla({ nome: "Lucas", idade: 22, altura: 1.75 });
+// saudarComOla({nome: "Lucas", idade: 22, altura: 1.75})
+pessoa.saudar("Skywaler");
--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -2,6 +2,7 @@ interface Humano {
   nome: string
   idade?: number //Opcional
   [prop: string]: any
+  saudar?(sobrenome: string): void
 }
 
 function saudarComOla(pessoa: Humano) {
@@ -14,11 +15,15 @@ function mudarNome(pessoa: Humano) {
 
 const pessoa = {
   nome: "Lucas",
-  idade: 22
+  idade: 22,
+  saudar(sobrenome: string){
+    console.log("Olá, meu nome é " + this.nome + " " + sobrenome)
+  }
 }
 
 saudarComOla(pessoa)
 mudarNome(pessoa)
 saudarComOla(pessoa)
-saudarComOla({nome: "Lucas", idade: 22, altura: 1.75})
+// saudarComOla({nome: "Lucas", idade: 22, altura: 1.75})
+pessoa.saudar("Skywaler")
 
